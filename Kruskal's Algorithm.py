438cb090ae851772bc862fd887b429df9408b1a1
Shyam Anand <67137678+ShyamAnand2@users.noreply.github.com>
2023-12-21 02:33:09 +0530

    Update Kruskal's Algorithm.py

--- Kruskal's Algorithm.py
+++ Kruskal's Algorithm.py
@@ -69,20 +69,6 @@ graph_2 = {
     '3': {'1': 2, '2': 3}
 }
 
-graph_3 = {
-    'X': {'Y': 2, 'Z': 1, 'W': 3},
-    'Y': {'X': 2, 'Z': 3, 'W': 1},
-    'Z': {'X': 1, 'Y': 3, 'W': 2},
-    'W': {'X': 3, 'Y': 1, 'Z': 2}
-}
-
-graph_4 = {
-    'M': {'N': 2, 'O': 3, 'P': 1},
-    'N': {'M': 2, 'O': 1, 'P': 4},
-    'O': {'M': 3, 'N': 1, 'P': 2},
-    'P': {'M': 1, 'N': 4, 'O': 2}
-}
-
 # Run Kruskal's algorithm on Sample Graphs, print the resulting minimum spanning tree, and plot the graphs
 min_spanning_tree_1 = kruskal(graph_1)
 print("Minimum Spanning Tree (Kruskal) - Sample Case 1:")
@@ -93,13 +79,3 @@ min_spanning_tree_2 = kruskal(graph_2)
 print("\nMinimum Spanning Tree (Kruskal) - Sample Case 2:")
 print(min_spanning_tree_2)
 plot_graph_with_edges(graph_2, [(u, v) for u, v, _ in min_spanning_tree_2], "Minimum Spanning Tree (Kruskal) - Sample Case 2")
-
-min_spanning_tree_3 = kruskal(graph_3)
-print("\nMinimum Spanning Tree (Kruskal) - Sample Case 3:")
-print(min_spanning_tree_3)
-plot_graph_with_edges(graph_3, [(u, v) for u, v, _ in min_spanning_tree_3], "Minimum Spanning Tree (Kruskal) - Sample Case 3")
-
-min_spanning_tree_4 = kruskal(graph_4)
-print("\nMinimum Spanning Tree (Kruskal) - Sample Case 4:")
-print(min_spanning_tree_4)
-plot_graph_with_edges(graph_4, [(u, v) for u, v, _ in min_spanning_tree_4], "Minimum Spanning Tree (Kruskal) - Sample Case 4")
