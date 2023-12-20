d4b99e2d9101250d98e3badebb8c3841ec91e193
Shyam Anand <67137678+ShyamAnand2@users.noreply.github.com>
2023-12-20 22:02:33 +0530

    Update Floyd-Warshall's Algorithm.py

--- Floyd-Warshall's Algorithm.py
+++ Floyd-Warshall's Algorithm.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec 20 21:09:11 2023
+Created on Wed Dec 20 21:40:01 2023
 
 @author: shyamanand
 """
-
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,57 +25,51 @@ def floyd_warshall(graph):
 
     return distance_matrix
 
-def print_final_result(result, graph_name):
-    print(f"Final Result for {graph_name}:\n", result)
-    print("\n\n")
+def draw_directed_graph(graph, pos, edge_labels, title):
+    plt.figure(figsize=(8, 6))
+    nx.draw(graph, pos, with_labels=True, node_size=700, font_size=10, font_color="white", node_color="gold", font_weight="bold", width=2)
 
-def draw_graph(graph, pos, edge_labels, title):
-    nx.draw(graph, pos, with_labels=True, node_size=700, font_size=10, font_color="white", node_color="skyblue", font_weight="bold", width=2, arrows=True)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
+    # Draw edges with arrowheads and edge labels
+    for edge, weight in edge_labels.items():
+        nx.draw_networkx_edges(graph, pos, edgelist=[edge], width=2, edge_color='black', connectionstyle='arc3,rad=0.1', arrowsize=15, arrowstyle='->')
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels={(edge[0], edge[1]): weight}, font_color='red')
+
     plt.title(title)
     plt.show()
 
-simple_graph = {'A': {'B': 1, 'C': 4}, 'B': {'A': 1, 'C': 2}, 'C': {'A': 4, 'B': 2}}
-vertices_1 = sorted(simple_graph)
-simple_graph_matrix = [[simple_graph.get(u, {}).get(v, np.inf) for v in vertices_1] for u in vertices_1]
-result_1 = floyd_warshall(simple_graph_matrix)
-print_final_result(result_1, "Simple Graph")
+# Sample directed graphs (all directed)
+simple_directed_graph = {'A': {'B': 2, 'C': 5, 'D': 1}, 'B': {'C': 1, 'D': 3}, 'C': {'D': 2}, 'D': {'A': 7}}
+medium_directed_graph = {'X': {'Y': 2, 'Z': 5}, 'Y': {'Z': 1, 'W': 3}, 'Z': {'W': 2}, 'W': {'X': 7}}
+complex_directed_graph = {'M': {'N': 2, 'O': 5}, 'N': {'O': 1, 'P': 3}, 'O': {'P': 2}, 'P': {'M': 7}}
 
-G1 = nx.DiGraph(simple_graph)
-pos1 = nx.spring_layout(G1)
-draw_graph(G1, pos1, nx.get_edge_attributes(G1, 'weight'), "Simple Graph (Floyd-Warshall Shortest Paths)")
+# Floyd-Warshall results
+vertices = sorted(simple_directed_graph)
+simple_directed_matrix = [[simple_directed_graph.get(u, {}).get(v, np.inf) for v in vertices] for u in vertices]
+result_simple_directed = floyd_warshall(simple_directed_matrix)
+print("Result:\n", result_simple_directed)
+print("\n\n")
 
-medium_graph = {'A': {'B': 2, 'C': 5}, 'B': {'A': 2, 'C': 1, 'D': 3}, 'C': {'A': 5, 'B': 1, 'D': 2}, 'D': {'B': 3, 'C': 2}}
-vertices_2 = sorted(medium_graph)
-medium_graph_matrix = [[medium_graph.get(u, {}).get(v, np.inf) for v in vertices_2] for u in vertices_2]
-result_2 = floyd_warshall(medium_graph_matrix)
-print_final_result(result_2, "Medium Complexity Graph")
+vertices = sorted(medium_directed_graph)
+medium_directed_matrix = [[medium_directed_graph.get(u, {}).get(v, np.inf) for v in vertices] for u in vertices]
+result_medium_directed = floyd_warshall(medium_directed_matrix)
+print("Result:\n", result_medium_directed)
+print("\n\n")
 
-G2 = nx.DiGraph(medium_graph)
-pos2 = nx.spring_layout(G2)
-draw_graph(G2, pos2, nx.get_edge_attributes(G2, 'weight'), "Medium Complexity Graph (Floyd-Warshall Shortest Paths)")
+vertices = sorted(complex_directed_graph)
+complex_directed_matrix = [[complex_directed_graph.get(u, {}).get(v, np.inf) for v in vertices] for u in vertices]
+result_complex_directed = floyd_warshall(complex_directed_matrix)
+print("Result:\n", result_complex_directed)
+print("\n\n")
 
-complex_graph = {'A': {'B': 2, 'C': 5}, 'B': {'A': 2, 'C': 1, 'D': 3, 'E': 6}, 'C': {'A': 5, 'B': 1, 'D': 2}, 'D': {'B': 3, 'C': 2, 'E': 4}, 'E': {'B': 6, 'D': 4, 'F': 8}, 'F': {'E': 8}}
-vertices_3 = sorted(complex_graph)
-complex_graph_matrix = [[complex_graph.get(u, {}).get(v, np.inf) for v in vertices_3] for u in vertices_3]
-result_3 = floyd_warshall(complex_graph_matrix)
-print_final_result(result_3, "More Nodes and Edges Graph")
+# Draw directed graphs with arrows and edge labels
+G_simple_directed = nx.DiGraph(simple_directed_graph)
+pos_simple_directed = nx.spring_layout(G_simple_directed)
+draw_directed_graph(G_simple_directed, pos_simple_directed, nx.get_edge_attributes(G_simple_directed, 'weight'), "Simple Directed Graph (Floyd-Warshall Shortest Paths)")
 
-G3 = nx.DiGraph(complex_graph)
-pos3 = nx.spring_layout(G3)
-draw_graph(G3, pos3, nx.get_edge_attributes(G3, 'weight'), "More Nodes and Edges Graph (Floyd-Warshall Shortest Paths)")
+G_medium_directed = nx.DiGraph(medium_directed_graph)
+pos_medium_directed = nx.spring_layout(G_medium_directed)
+draw_directed_graph(G_medium_directed, pos_medium_directed, nx.get_edge_attributes(G_medium_directed, 'weight'), "Medium Directed Graph (Floyd-Warshall Shortest Paths)")
 
-directed_graph = {
-    'A': {'B': 2, 'C': 5},
-    'B': {'C': 1, 'D': 3},
-    'C': {'D': 2},
-    'D': {'A': 7} 
-}
-vertices_4 = sorted(directed_graph)
-directed_graph_matrix = [[directed_graph.get(u, {}).get(v, np.inf) for v in vertices_4] for u in vertices_4]
-result_4 = floyd_warshall(directed_graph_matrix)
-print_final_result(result_4, "Directed Graph")
-
-G4 = nx.DiGraph(directed_graph)
-pos4 = nx.spring_layout(G4)
-draw_graph(G4, pos4, nx.get_edge_attributes(G4, 'weight'), "Directed Graph (Floyd-Warshall Shortest Paths)")
+G_complex_directed = nx.DiGraph(complex_directed_graph)
+pos_complex_directed = nx.spring_layout(G_complex_directed)
+draw_directed_graph(G_complex_directed, pos_complex_directed, nx.get_edge_attributes(G_complex_directed, 'weight'), "Complex Directed Graph (Floyd-Warshall Shortest Paths)")
